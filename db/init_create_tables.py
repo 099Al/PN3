6a9dcf8ba8cb77cb5a66aacd8a64877729c8250a
f999t1 <f999t1@yandex.ru>
2023-07-21 11:52:39 +0600

    init scripts

--- db/init_create_tables.py
+++ db/init_create_tables.py
@@ -1,6 +1,8 @@
 #Создание таблиц в Базе
 
-"""CREATE TABLE cex_history_tik (
+queries_d ={
+'history_tik':
+    """CREATE TABLE cex_history_tik (
     tid        BIGINT          PRIMARY KEY ASC ON CONFLICT ROLLBACK,
     type       CHAR (6),
     unixdate   BIGINT,
@@ -8,26 +10,30 @@
     amount     DECIMAL (11, 8),
     price      DECIMAL (9, 2),
     sys_insert DATETIME
-);"""
-,"""CREATE TABLE im_cex_history_tik (
+)"""
+
+, 'im_history_tik':
+    """CREATE TABLE im_cex_history_tik (
     tid      BIGINT          PRIMARY KEY ASC ON CONFLICT ROLLBACK,
     type     CHAR (6),
     unixdate BIGINT,
     date     DATETIME,
     amount   DECIMAL (11, 8),
     price    DECIMAL (9, 2) 
-);
-"""
-,"""CREATE TABLE stg_cex_history_tik (
+)"""
+
+,'stg_history_tik':
+    """CREATE TABLE stg_cex_history_tik (
     tid      BIGINT,
     type     CHAR (6),
     unixdate BIGINT,
     date     DATETIME,
     amount   DECIMAL (11, 8),
     price    DECIMAL (9, 2) 
-);
-"""
-,"""CREATE TABLE active_orders (
+)"""
+
+,'active_orders':
+    """CREATE TABLE active_orders (
     id         INTEGER        PRIMARY KEY,
     unix_date  INTEGER,
     pair       VARCHAR (20)   CHECK (pair IN ('BTC/USD') ),
@@ -36,8 +42,19 @@
     price      DOUBLE (20, 4),
     order_type VARCHAR (10)   CHECK (order_type IN ('market', 'limit') ),
     sys_date   DATETIME
-);
-"""
+)"""
+}
 #Очистка таблиц перед тестированием
 
 
+if __name__ == '__main__':
+    from db.connection import DBConnect
+
+    conn = DBConnect().getConnect()
+    cursor = conn.cursor()
+
+    #INIT DB
+    #for sql in queries_d.values():
+    #    cursor.execute(sql)
+
+    #CLEAR TABLES
