54b84dbdbbfd7c94e978407a18ba78fd60eee382
099Al <alexef157@gmail.com>
2023-08-02 10:12:57 +0600

    changes

---
+++ algorithms/a1/manager.py
@@ -0,0 +1,23 @@
+
+
+
+def f_alg1():
+    if MODE == "TRADE"
+
+        api = API
+
+    if MODE == 'TEST':
+
+        api = API_TEST
+
+
+    values = calculations
+
+    res = set (price,amount,side)
+
+    save res into db (transactons, curr_state)
+
+
+
+
+api = Api(config.API_USER, config.API_KEY, config.API_SECRET)
