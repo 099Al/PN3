cfab39bef6d7ca4271d29623de4daed305d219a6
efimov_as <efimov_as@tochka.com>
2026-01-18 11:56:36 +0500

    schema emulator

--- src/run_traiding/run.py
+++ src/run_traiding/run.py
@@ -1,8 +1,6 @@
 import asyncio
+from datetime import datetime
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-from src.database.connect import DataBase
 from src.run_traiding.balances_init import set_balance
 
 
@@ -11,9 +9,28 @@ l_algos = [
     {"name": "algo_2", "usd": 0,   "btc": 0.2},
 ]
 
+t_start = '2023-07-22 15:00:00'
 
+period =
+
+
+def traiding():
+
+    t_start_unix = int(datetime.strptime(t_start, '%Y-%m-%d %H:%M:%S').timestamp())
+
+    curr_unix_time = t_start_unix
+
+    n = 0
+    curr_unix_time = curr_unix_time
+
+    while True:
+        n = n + 1
+        curr_unix_time = curr_unix_time + period
 
 
 if __name__ == '__main__':
 
-    asyncio.run(set_balance(l_algos))
+    # asyncio.run(set_balance(l_algos))
+
+    traiding()
+
