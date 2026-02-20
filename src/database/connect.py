80494168b8d2f01e7212a44a907aceb721a0ed04
efimov_as <efimov_as@tochka.com>
2026-02-20 10:25:54 +0500

    connect

--- src/database/connect.py
+++ src/database/connect.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from sqlalchemy import select, text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
@@ -13,13 +15,49 @@ class DataBase:
             cls.instance = super(DataBase, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
+    def __init__(self, echo=False):
         if hasattr(self, "_initialized"):
             return
         self.connect = prj_configs.connect_url
-        self.async_engine = create_async_engine(self.connect, echo=False)
-        self.session_maker = async_sessionmaker(bind=self.async_engine, class_=AsyncSession) #, expire_on_commit=False,
+        self.async_engine = create_async_engine(
+            self.connect,
+            echo=echo,
+            pool_pre_ping=True
+        )
+        self.session_maker = async_sessionmaker(
+            bind=self.async_engine,
+            class_=AsyncSession,
+            # , expire_on_commit=False,   чтобы не ловить MissingGreenlet
+        )
         self._initialized = True
+
+
+    def get_engine(self):
+        return self.async_engine
+
+
+    def get_session_maker(self):
+        return self.session_maker
+
+    @asynccontextmanager
+    async def session(self) -> AsyncSession:
+        """
+        автоматический rollback при ошибке
+        гарантированный close
+        """
+        async with self.session_maker() as session:
+            try:
+                yield session
+            except:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
+
+    async def dispose(self):
+        # Закрывает все активные соединения
+        await self.async_engine.dispose()
+
 
     async def check_connection(self) -> bool:
         try:
@@ -31,26 +69,15 @@ class DataBase:
             print(f"DB connection error: {e}")
             return False
 
-    def get_session_maker(self):
-        return self.session_maker
-
-
-    def get_engine(self):
-        return self.async_engine
-
-    async def create_db(self):
-        async with self.async_engine.begin() as connect:
-            await connect.run_sync(Base.metadata.create_all)
-
     async def get_table(self, table_name):
         async with self.session_maker() as session:
             result = await session.execute(select(table_name))
         return result.scalars().all()
 
-    async def get_db(self) -> AsyncSession:
-        async_session = DataBase().get_session_maker()
-        async with async_session() as session:
-            yield session
+
+    async def create_db(self):
+        async with self.async_engine.begin() as connect:
+            await connect.run_sync(Base.metadata.create_all)
 
 if __name__ == '__main__':
 
